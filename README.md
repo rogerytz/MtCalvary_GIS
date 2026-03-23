590904e1571ba033353aca8e486ad6de640f7e05
rogerytz <142941118+rogerytz@users.noreply.github.com>
2026-03-22 23:06:45 -0400

    Updated  2026-03-22
    Scale 1:1200

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # MtCalvary\_GIS
 
 Cemetery GIS Map -->
@@ -28,3 +29,45 @@ Update March 17, 2026
 
 &#x20;       Spatial Bookmark 2.  up left center
 
+=======
+# MtCalvary\_GIS
+
+Cemetery GIS Map -->
+qgis2Web (Open Layers) files
+
+Updated January 20, 2026
+Used Bookmark to center.  Scale 1:600
+
+Updated January 25, 2026
+
+   Scale 1:1000
+
+Updated January 26, 2026
+
+   Scale 1:1000, No Legend
+
+Updated March 12, 2026
+
+   Scale 1:1000, No Legend
+
+   Spatial bookmark “qgis2web scale 1:1000”
+
+Updated March 15,2026
+
+&#x09;Spatial bookmark Centered sale 1:1000
+
+Update March 17, 2026
+
+&#x20;       Spatial Bookmark 2.  up left center  Scale 1:1340
+
+&#x09;xmin -73.990058  ymin 41.941629
+
+Update March 22, 2026
+
+&#x09;Spatial Bookmark 3.  Scale 1:1200
+
+&#x09;xmin -73.989726  ymin 41.941484
+
+
+
+>>>>>>> Stashed changes
--- layers/layers.js
+++ layers/layers.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ol.proj.proj4.register(proj4);
 //ol.proj.get("EPSG:4326").setExtent([-73.990058, 41.941629, -73.983933, 41.944953]);
 var wms_layers = [];
@@ -44,4 +45,52 @@ lyr_Cemetery_0.set('fieldLabels', {'fid': 'no label', 'id': 'no label', 'section
 lyr_Interred_1.set('fieldLabels', {'Namel': 'no label', 'LFname': 'no label', 'Deceased': 'inline label - always visible', 'Age': 'inline label - always visible', });
 lyr_Interred_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
+=======
+ol.proj.proj4.register(proj4);
+//ol.proj.get("EPSG:4326").setExtent([-73.989726, 41.941484, -73.984597, 41.944884]);
+var wms_layers = [];
+
+var format_Cemetery_0 = new ol.format.GeoJSON();
+var features_Cemetery_0 = format_Cemetery_0.readFeatures(json_Cemetery_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
+var jsonSource_Cemetery_0 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Cemetery_0.addFeatures(features_Cemetery_0);
+var lyr_Cemetery_0 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Cemetery_0, 
+                style: style_Cemetery_0,
+                popuplayertitle: 'Cemetery',
+                interactive: false,
+                title: '<img src="styles/legend/Cemetery_0.png" /> Cemetery'
+            });
+var format_Interred_1 = new ol.format.GeoJSON();
+var features_Interred_1 = format_Interred_1.readFeatures(json_Interred_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
+var jsonSource_Interred_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Interred_1.addFeatures(features_Interred_1);
+var lyr_Interred_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Interred_1, 
+                style: style_Interred_1,
+                popuplayertitle: 'Interred',
+                interactive: true,
+    title: 'Interred<br />\
+    <img src="styles/legend/Interred_1_0.png" /> Single Burial<br />\
+    <img src="styles/legend/Interred_1_1.png" /> Multiple Burials<br />' });
+
+lyr_Cemetery_0.setVisible(true);lyr_Interred_1.setVisible(true);
+var layersList = [lyr_Cemetery_0,lyr_Interred_1];
+lyr_Cemetery_0.set('fieldAliases', {'fid': 'fid', 'qc_id': 'qc_id', 'id': 'id', 'section': 'section', 'block': 'block', 'boarder': 'boarder', 'label': 'label', 'auxiliary_': 'auxiliary_', 'auxiliar_1': 'auxiliar_1', 'auxiliar_2': 'auxiliar_2', });
+lyr_Interred_1.set('fieldAliases', {'Key': 'Key', 'Section': 'Section', 'MapLgnd': 'MapLgnd', 'BurialCnt': 'BurialCnt', 'Namel': 'Namel', 'LFname': 'LFname', 'Deceased': 'Deceased', 'Age': 'Age', 'xLong': 'xLong', 'YLat': 'YLat', 'StatusCode': 'StatusCode', });
+lyr_Cemetery_0.set('fieldImages', {'fid': 'TextEdit', 'qc_id': 'Hidden', 'id': 'TextEdit', 'section': 'TextEdit', 'block': 'TextEdit', 'boarder': 'TextEdit', 'label': 'TextEdit', 'auxiliary_': 'Hidden', 'auxiliar_1': 'Hidden', 'auxiliar_2': 'Hidden', });
+lyr_Interred_1.set('fieldImages', {'Key': 'Hidden', 'Section': 'Hidden', 'MapLgnd': 'Hidden', 'BurialCnt': 'Hidden', 'Namel': 'TextEdit', 'LFname': 'TextEdit', 'Deceased': 'TextEdit', 'Age': 'Range', 'xLong': 'Hidden', 'YLat': 'Hidden', 'StatusCode': 'Hidden', });
+lyr_Cemetery_0.set('fieldLabels', {'fid': 'no label', 'id': 'no label', 'section': 'no label', 'block': 'no label', 'boarder': 'no label', 'label': 'no label', });
+lyr_Interred_1.set('fieldLabels', {'Namel': 'no label', 'LFname': 'no label', 'Deceased': 'inline label - always visible', 'Age': 'inline label - always visible', });
+lyr_Interred_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+>>>>>>> Stashed changes
 });
